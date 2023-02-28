dd54b448ba650362bd1bd8c190e7cef579d2d3cd
Swadhin Ghosh <43653792+Swadhincse@users.noreply.github.com>
2023-02-28 18:16:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const h3Element = document.querySelector("#wordCount");
 
 function handleInput(e){
     const sentence = e.target.value;
-    const count = sentence.split(" ").length;
-    h3Element.textContent = count;
+    const match = sentence.match(/\S+/ig).length;
+    h3Element.textContent = match;
 }
 textElement.addEventListener('input', handleInput);
