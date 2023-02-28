5206ac01ae52c091ca7be598c6c3043acf9dea2e
Swadhin Ghosh <43653792+Swadhincse@users.noreply.github.com>
2023-02-28 18:06:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your code here
+const textElement = document.querySelector("#evaluatedText");
+const h3Element = document.querySelector("#wordcount");
+
+function handleInput(e){
+    const sentence = e.target.value;
+    const count = sentence.split(" ").length;
+    h3Element.textContent = count-1;
+}
+textElement.addEventListener('input', handleInput);
